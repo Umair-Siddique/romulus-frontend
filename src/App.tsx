5b6ef6151845f6debfad7896f1380c523dc809a1
Sharjeel Faiq <mrmalik610@gmail.com>
2025-06-14 12:35:54 +0500

    Refactors App component structure and removes unused ColorModeContext

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import Dashboard from "@mui/icons-material/Dashboard";
 import Box from "@mui/material/Box";
 import { authProvider } from "./providers";
 import { DashboardPage } from "./pages/dashboard";
-import { ColorModeContextProvider } from "./contexts";
 import { Header, Title } from "./components";
 import {
   LoginPage,
@@ -29,98 +28,96 @@ import {
   UpdatePasswordPage,
 } from "./pages/auth";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <KBarProvider>
-        <ColorModeContextProvider>
-          <CssBaseline />
-          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-          <RefineSnackbarProvider>
-            <Refine
-              routerProvider={routerProvider}
-              dataProvider={dataProvider(API_URL)}
-              authProvider={authProvider}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                breadcrumb: false,
-                useNewQueryKeys: true,
-              }}
-              notificationProvider={useNotificationProvider}
-              resources={[
-                {
-                  name: "dashboard",
-                  list: "/",
-                  meta: {
-                    label: "Dashboard",
-                    icon: <Dashboard />,
-                  },
+        <CssBaseline />
+        <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+        <RefineSnackbarProvider>
+          <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider(API_URL)}
+            authProvider={authProvider}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              breadcrumb: false,
+              useNewQueryKeys: true,
+            }}
+            notificationProvider={useNotificationProvider}
+            resources={[
+              {
+                name: "dashboard",
+                list: "/",
+                meta: {
+                  label: "Dashboard",
+                  icon: <Dashboard />,
                 },
-              ]}
-            >
-              <Routes>
-                <Route
-                  element={
-                    <Authenticated
-                      key="authenticated-routes"
-                      fallback={<CatchAllNavigate to="/login" />}
-                    >
-                      <ThemedLayoutV2 Header={Header} Title={Title}>
-                        <Box
-                          sx={{
-                            maxWidth: "1200px",
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                          }}
-                        >
-                          <Outlet />
-                        </Box>
-                      </ThemedLayoutV2>
-                    </Authenticated>
-                  }
-                >
-                  <Route index element={<DashboardPage />} />
-                </Route>
-
-                <Route
-                  element={
-                    <Authenticated key="auth-pages" fallback={<Outlet />}>
-                      <NavigateToResource resource="dashboard" />
-                    </Authenticated>
-                  }
-                >
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/register" element={<RegisterPage />} />
-                  <Route
-                    path="/forgot-password"
-                    element={<ForgotPasswordPage />}
-                  />
-                  <Route
-                    path="/update-password"
-                    element={<UpdatePasswordPage />}
-                  />
-                </Route>
-
-                <Route
-                  element={
-                    <Authenticated key="catch-all">
-                      <ThemedLayoutV2 Header={Header} Title={Title}>
+              },
+            ]}
+          >
+            <Routes>
+              <Route
+                element={
+                  <Authenticated
+                    key="authenticated-routes"
+                    fallback={<CatchAllNavigate to="/login" />}
+                  >
+                    <ThemedLayoutV2 Header={Header} Title={Title}>
+                      <Box
+                        sx={{
+                          maxWidth: "1200px",
+                          marginLeft: "auto",
+                          marginRight: "auto",
+                        }}
+                      >
                         <Outlet />
-                      </ThemedLayoutV2>
-                    </Authenticated>
-                  }
-                >
-                  <Route path="*" element={<ErrorComponent />} />
-                </Route>
-              </Routes>
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-          </RefineSnackbarProvider>
-        </ColorModeContextProvider>
+                      </Box>
+                    </ThemedLayoutV2>
+                  </Authenticated>
+                }
+              >
+                <Route index element={<DashboardPage />} />
+              </Route>
+
+              <Route
+                element={
+                  <Authenticated key="auth-pages" fallback={<Outlet />}>
+                    <NavigateToResource resource="dashboard" />
+                  </Authenticated>
+                }
+              >
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route
+                  path="/forgot-password"
+                  element={<ForgotPasswordPage />}
+                />
+                <Route
+                  path="/update-password"
+                  element={<UpdatePasswordPage />}
+                />
+              </Route>
+
+              <Route
+                element={
+                  <Authenticated key="catch-all">
+                    <ThemedLayoutV2 Header={Header} Title={Title}>
+                      <Outlet />
+                    </ThemedLayoutV2>
+                  </Authenticated>
+                }
+              >
+                <Route path="*" element={<ErrorComponent />} />
+              </Route>
+            </Routes>
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+        </RefineSnackbarProvider>
       </KBarProvider>
     </BrowserRouter>
   );
--- src/components/drawer/drawer/index.tsx
+++ src/components/drawer/drawer/index.tsx
@@ -1,19 +1,16 @@
 import BaseDrawer, { type DrawerProps } from "@mui/material/Drawer";
 import type { PropsWithChildren } from "react";
 import gray from "@mui/material/colors/grey";
-import { useColorModeContext } from "../../../contexts";
 
 type Props = {} & DrawerProps;
 
 export const Drawer = ({ children, ...props }: PropsWithChildren<Props>) => {
-  const { mode } = useColorModeContext();
-
   return (
     <BaseDrawer
       {...props}
       sx={{
         "& .MuiDrawer-paper": {
-          backgroundColor: mode === "light" ? gray[100] : "#000",
+          backgroundColor: gray[100],
         },
         ...props.sx,
       }}
