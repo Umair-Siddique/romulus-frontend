7b315559e5636e954a08b081ad88c509c75aebdd
Sharjeel Faiq <mrmalik610@gmail.com>
2025-06-16 17:59:13 +0500

    feat: Add profile dropdown with logout functionality in PageHeader component

--- src/components/createProfile/pageHeader/index.tsx
+++ src/components/createProfile/pageHeader/index.tsx
@@ -1,9 +1,34 @@
-import React from "react";
-import { AppBar, Box, Toolbar } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { AccountCircle, ArrowDropDown } from "@mui/icons-material";
 
 import Logo from "../../../assets/images/logo.png";
+import { useLogout } from "@refinedev/core";
 
 export const PageHeader: React.FC = () => {
+  const { mutate: logout } = useLogout();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    logout();
+    handleClose();
+  };
+
   return (
     <AppBar
       position="static"
@@ -17,6 +42,37 @@ export const PageHeader: React.FC = () => {
         {/* Logo */}
         <Box sx={{ textAlign: "center" }}>
           <Box component="img" src={Logo} alt="Logo" width={40} height={40} />
+        </Box>
+
+        {/* Profile Dropdown */}
+        <Box>
+          <IconButton
+            onClick={handleProfileClick}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              color: "#666",
+            }}
+          >
+            <AccountCircle sx={{ mr: 0.5 }} />
+            <ArrowDropDown />
+          </IconButton>
+
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
