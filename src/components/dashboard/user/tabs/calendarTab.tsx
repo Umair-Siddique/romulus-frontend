e2967b97613f5afd8b18f063e9d75e1856bbb678
Sharjeel Faiq <mrmalik610@gmail.com>
2025-07-04 01:11:25 +0500

    feat: Add calendar functionality with event management and integrate react-big-calendar

--- src/components/dashboard/user/tabs/calendarTab.tsx
+++ src/components/dashboard/user/tabs/calendarTab.tsx
@@ -1,16 +1,45 @@
+import React, { useState } from "react";
 import { Typography } from "@mui/material";
-import React from "react";
 import { useTheme, Theme } from "@mui/material/styles";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import events from "./events";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
+moment.locale("en-GB");
 export const CalendarTab: React.FC = () => {
   const theme = useTheme<Theme>();
+  const [eventsData, setEventsData] = useState(events);
+  const localizer = momentLocalizer(moment);
+
+  const handleSelect = ({ start, end }: any) => {
+    console.log(start);
+    console.log(end);
+    const title = window.prompt("New Event name");
+    if (title)
+      setEventsData([
+        ...eventsData,
+        {
+          start,
+          end,
+          title,
+        },
+      ]);
+  };
 
   return (
     <>
-      <Typography variant="h6">Calendar View</Typography>
-      <Typography variant="body2" color="text.secondary">
-        Calendar component will be loaded here
-      </Typography>
+      <Calendar
+        selectable
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        views={["month"]} // lock to month view only
+        events={eventsData}
+        style={{ height: "100vh" }}
+        onSelectEvent={(event) => alert(event.title)}
+        onSelectSlot={handleSelect}
+      />
     </>
   );
 };
