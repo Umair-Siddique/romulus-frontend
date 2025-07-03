95d56698184c4993438fcb3809342182dcbf9c98
Sharjeel Faiq <mrmalik610@gmail.com>
2025-07-03 20:19:06 +0500

    refactor: Remove unused imports from Dashboard component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
 import { useUserContext } from "../../context";
-import { useNavigate } from "react-router";
 
 export const Dashboard = () => {
   const { user } = useUserContext();
