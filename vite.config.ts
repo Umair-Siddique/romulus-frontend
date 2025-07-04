885d7518942840e22d84d2d0b12821cdd5c65e94
Sharjeel Faiq <mrmalik610@gmail.com>
2025-07-04 22:43:30 +0500

    fix: Update tsconfig and vite config for improved path aliases and organization

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "#assets": path.resolve(__dirname, "src/assets"),
+      "#components": path.resolve(__dirname, "src/components"),
+      "#constants": path.resolve(__dirname, "src/constants"),
+      "#context": path.resolve(__dirname, "src/context"),
+      "#pages": path.resolve(__dirname, "src/pages"),
+      "#providers": path.resolve(__dirname, "src/providers"),
+      "#types": path.resolve(__dirname, "src/types"),
+      "#utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
 });
