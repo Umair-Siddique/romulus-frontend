28518b5140fd44e37fa129055479f16d5c261bc3
Sharjeel Faiq <mrmalik610@gmail.com>
2025-07-06 18:04:37 +0500

    feat: Add form configuration, fields, and modal handling for registration process

--- src/pages/auth/Register/index.tsx
+++ src/pages/auth/Register/index.tsx
@@ -5,9 +5,9 @@ import { useCustom, useRegister } from "@refinedev/core";
 
 import AuthBg from "/images/auth-bg.jpg";
 import { Modal } from "#components";
-import { getFormFields } from "./formFields";
-import { getFormConfig } from "./formConfig";
-import { getModalConfig } from "./modalConfig";
+import { getFormFields } from "./FormFields";
+import { getFormConfig } from "./FormConfig";
+import { getModalConfig } from "./ModalConfig";
 import { AuthBackground, AuthForm } from "../../../components/auth";
 
 import { RegisterVariables } from "#types";
