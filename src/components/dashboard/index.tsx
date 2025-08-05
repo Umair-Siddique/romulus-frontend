15c9074cfc38152ae5fd30fbeaa3eb2ef06ea5df
Sharjeel Faiq <mrmalik610@gmail.com>
2025-08-05 21:58:57 +0500

    Fixed some conditional logic

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -147,22 +147,22 @@ export const UserDashboard = ({
     return getMissionCounts(missions, pendingInvitations);
   }, [missions, role, userProfile?.missionsInvitedFor]);
 
-  const invitedPlusHiredMissions = [
-    ...userProfile?.missionsInvitedFor.map((mission: any) => mission.mission),
-    ...userProfile?.missionsHiredFor,
-  ].filter(
-    (mission: any, index: number, self: any) =>
-      self.findIndex((t: any) => t._id === mission._id) === index
-  );
-
   // Optimized KPIs calculation
   const kpis = useMemo(() => {
     const totalMissions =
-      role !== "educator"
-        ? missionsData?.total || 0
-        : invitedPlusHiredMissions?.length || 0;
+      role === "educator"
+        ? [
+            ...userProfile?.missionsInvitedFor?.map(
+              (mission: any) => mission.mission
+            ),
+            ...userProfile?.missionsHiredFor,
+          ].filter(
+            (mission: any, index: number, self: any) =>
+              self.findIndex((t: any) => t._id === mission._id) === index
+          )?.length
+        : missionsData?.total;
 
-    return getFilteredKpis(role!, missionCounts, totalMissions);
+    return getFilteredKpis(role!, missionCounts, totalMissions!);
   }, [
     role,
     missionsData?.total,
